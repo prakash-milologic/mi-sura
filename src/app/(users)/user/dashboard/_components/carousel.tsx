db1e39cd71b0127aec946d99ec062b7c09a8b762
Ram Shrestha <acharyaprakash030@gmail.com>
2025-01-01 20:33:34 +0545

    fix slider

--- src/app/(users)/user/dashboard/_components/carousel.tsx
+++ src/app/(users)/user/dashboard/_components/carousel.tsx
@@ -3,6 +3,8 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider, { Settings } from "react-slick";
 
+import React from 'react';
+
 export default function Carousel({ children }: { children: React.ReactNode }) {
   const settings: Settings = {
     dots: true,
